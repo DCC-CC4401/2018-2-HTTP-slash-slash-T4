cb8394088621d4dc3be22c114afd69e998ba4667
Jose Vergara <vergarajose365@gmail.com>
2018-11-07 19:25:37 -0300

    Se agregan modelos para la base de datos
    Los modelos agregados son: Usuario, Info_Coevaluacion,
    Equipo, Integrante_Equipo, Admin y Coevaluacion.

--- coev/models.py
+++ coev/models.py
@@ -1,3 +1,54 @@
 from django.db import models
 
 # Create your models here.
+
+class Usuario(models.Model):
+    rut = models.CharField(max_length=10, 
+                            primary_key=True, 
+                            null=False, 
+                            blank=False, 
+                            help_text='Ingrese rut sin puntos ni guión', 
+                            unique=True)
+    nombre = models.CharField(null=False, blank=False)
+    correo = models.EmailField(null=False, blank=False)
+
+class Equipo(models.Model):
+    curso_id = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    nombre = models.CharField(null=False, blank=False)
+    historial = models.TextField(null=True, blank=True, unique=False)
+
+    class Meta:
+        unique_together= (("curso_id", "nombre"),)
+
+class Info_Coevaluacion(models.Model):
+    curso_id = models.ForeignKey(Curso, on_delete=models.CASCADE)
+    numero = models.ForeignKey(Cevaluacion, to_field="numero", on_delete=models.CASCADE)
+    rut_usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    respondida = models.BooleanField(null=False)
+    nota = models.FloatField(max_length=3, null=True, blank=True)
+
+    class Meta:
+        unique_together= (("curso_id", "numero", "rut"),)
+
+class Integrante_Equipo(models.Model):
+    equipo_id= models.ForeignKey(Equipo, on_delete=models.CASCADE)
+    curso_id= models.ForeignKey(Curso, on_delete=models.CASCADE)
+    rut= models.ForeignKey(Usuario, on_delete=models.CASCADE) 
+    activo= models.BooleanField(default=True)
+
+    class Meta:
+        unique_together= (("equipo_id", "curso_id", "rut"),)
+
+class Admin(models.Model):
+    usuario_rut= models.ForeignKey(Usuario, on_delete=models.CASCADE)
+
+
+class Coevaluacion(models.Model):
+    curso_id= models.ForeignKey(curso_id, on_delete=models.CASCADE)
+    numero = models.IntegerField(max_length=2, null=False, blank=False, unique=False)
+    fecha_inicio= models.DateField(null=False, blank=False) 
+    fecha_fin= models.DateField(null=False, blank=False) 
+    opciones= (("abierta", "Abierta"),
+                ("cerrada", "Cerrada"),
+                ("publicada", "Publicada"))
+    estado= models.CharField(null=False, blank=False,choices= opciones)
