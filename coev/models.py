6b2f2efcd4338c7fedf26871c939c0e2d4d1e924
jberroca <berrocaljo@gmail.com>
2018-11-07 18:34:10 -0300

    se agrego exclusividad mutua para integrante_curso

--- coev/models.py
+++ coev/models.py
@@ -2,9 +2,19 @@ from django.db import models
 
 # Create your models here.
 class Integrante_Curso(models.Model):
+
     rut=models.ForeignKey(Usuario)
     curso=models.ForeignKey(curso.id)
     rol=models.charField(max_length=30)
-        def __str__(self):
+    class Meta:
+        unique_together=(("rut","curso"),)
+    def __str__(self):
         return self.rut
-        
+
+class curso(models.Model):
+    nombre=models.charField(max_length=10)
+    seccion=models.IntegerField(default=1)
+    anho=models.integerField()
+    semestre=models.integerField()
+    def __str__(self):
+        return self.nombre"-"+self.seccion", semestre"+self.semestre+self.anho
