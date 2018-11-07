c3aebd1774a42195917fe130e152a3dc4ef4a577
jberroca <berrocaljo@gmail.com>
2018-11-07 18:39:31 -0300

    se agrego exclusividad mutua para integrante_curso

--- coev/models.py
+++ coev/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.core.validators import MaxValueValidator, MinValueValidator
 # Create your models here.
 class Integrante_Curso(models.Model):
 
@@ -12,9 +12,11 @@ class Integrante_Curso(models.Model):
         return self.rut
 
 class curso(models.Model):
-    nombre=models.charField(max_length=10)
+    nombre=models.charField(max_length=10,not)
     seccion=models.IntegerField(default=1)
     anho=models.integerField()
-    semestre=models.integerField()
+    semestre=models.integerField(
+        validators=[MaxValueValidator(3), MinValueValidator(1)]
+    )
     def __str__(self):
         return self.nombre"-"+self.seccion", semestre"+self.semestre+self.anho
