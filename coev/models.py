adabaee7b9bde5a7ae49794312b9a96260725dca
jberroca <berrocaljo@gmail.com>
2018-11-07 16:22:21 -0300

    se agrego Integrante_curso

--- coev/models.py
+++ coev/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Integrante_Curso(models.Model){
+    rut=models.ForeignKey(Usuario)
+    curso=models.ForeignKey(curso.id)
+    rol=models.charField(max_length=30)
+        def __str__(self):
+        return self.rut
+
+    
+}
